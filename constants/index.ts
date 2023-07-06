0e9ec053de3f35129ae3e22abdcd0e9ed86932aa
Damien Robinson <damien.robinson@daim.dev>
2023-07-06 11:32:07 +1000

    chore(constants): update name and description

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Battle Calculator'
+export const appDescription = 'Tabletop battle simulation'
