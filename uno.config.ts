665c9cba1ff11cbe0c8e1e20bff0598dc3f07e71
Damien Robinson <damien.robinson@daim.dev>
2023-07-07 18:05:37 +1000

    feat: add heading 2 style

--- uno.config.ts
+++ uno.config.ts
@@ -11,6 +11,8 @@ import {
 
 export default defineConfig({
   shortcuts: [
+    ['h3', 'text-3xl font-bold dark:text-white'],
+    ['h2', 'text-4xl font-extrabold dark:text-white'],
     ['input', 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'],
     ['h1', 'mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'],
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
