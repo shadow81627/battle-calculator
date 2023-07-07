66641346d27b59680748e3834d5d786c995a7560
Damien Robinson <damien.robinson@daim.dev>
2023-07-07 21:53:53 +1000

    feat(uno css): add input disabled short cut

--- uno.config.ts
+++ uno.config.ts
@@ -11,6 +11,7 @@ import {
 
 export default defineConfig({
   shortcuts: [
+    ['input-disabled', 'bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500'],
     ['h3', 'text-3xl font-bold dark:text-white'],
     ['h2', 'text-4xl font-extrabold dark:text-white'],
     ['input', 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'],
