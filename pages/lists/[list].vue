4c61038e6ecc5e517c0282b8c528a7bcc4696d6b
Damien Robinson <damien.robinson@daim.dev>
2023-07-08 10:41:49 +1000

    refactor(list): move squad to own files

--- pages/lists/[list].vue
+++ pages/lists/[list].vue
@@ -1,12 +1,12 @@
 <script setup>
 const { params } = useRoute()
 const id = params.list
-const { data } = await useAsyncData(`lists-${id}`, () => queryContent('lists', id).findOne())
+const { data } = await useAsyncData(`lists-${id}`, () => queryContent('lists', id).find())
 </script>
 
 <template>
-  <h1 class="h2 text-left">
-    {{ data.title }} List
+  <h1 class="h2 text-left capitalize">
+    {{ id }} List
   </h1>
-  <ArmyList :data="data.body" />
+  <ArmyList :data="data" />
 </template>
