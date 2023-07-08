4feff8cb1b0aeb64379a891693fd740ad9ed1cc6
Damien Robinson <damien.robinson@daim.dev>
2023-07-08 10:10:53 +1000

    feat(layout): add links

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,12 @@
 <template>
+  <header>
+    <nuxt-link to="/">
+      Home
+    </nuxt-link>
+    <nuxt-link to="/lists/damien">
+      Damien's list
+    </nuxt-link>
+  </header>
   <main class="px-10 py-20 text-center">
     <slot />
   </main>
