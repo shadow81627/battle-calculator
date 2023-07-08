e3aa98731da21d635ff6a6fa6ac516fdd8d758df
Damien Robinson <damien.robinson@daim.dev>
2023-07-08 13:41:05 +1000

    feat: add turns and add attacker data fetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <script setup>
-const strength = ref(12)
+const id = 'sentinel'
+const { data: attacker } = await useAsyncData('home', () => queryContent('lists/damien', id).findOne())
 
-const attack = ref(18)
-const accuracy = ref(3)
-const piercing = ref(2)
-const damage = ref(4)
+const weapon = attacker.value.weapons[0]
+
+const attack = ref(3 * weapon.attack)
+const accuracy = ref(weapon.accuracy)
+const strength = ref(weapon.strength)
+const piercing = ref(weapon.piercing)
+const damage = ref(weapon.damage)
 
 const toughness = ref(10)
 const wound = ref(13)
 const save = ref(4)
 const invulnerable = ref(5)
 const pain = ref(5)
+
+const turns = ref(2)
 </script>
 
 <template>
@@ -25,10 +31,7 @@ const pain = ref(5)
             <h2 class="h2">
               Attacker Statistics
             </h2>
-            <p>3x 3 Rapier Lasers (90pts) + Lord Solar Leontus (125pts)</p>
-            <p>
-              Total Points {{ (3 * 90) + 125 }}pts
-            </p>
+            <p>{{ attacker.models }} {{ attacker.name }} {{ attacker.points }}pts</p>
           </div>
           <Attributes
             v-model:attack="attack"
@@ -40,19 +43,13 @@ const pain = ref(5)
             :save="4"
             :wound="3"
           />
-          <p class="text-left">
-            Take aim improves accuracy from 5+ to 4+, then heavy improves the accuracy to 3+.
-          </p>
         </div>
         <div class="col-6">
           <div class="text-left">
             <h2 class="h2">
               Defender Statistics
             </h2>
-            <p>1 Mutalith Vortex Beast</p>
-            <p>
-              Total Points {{ 145 }}pts
-            </p>
+            <p>1 Mutalith Vortex Beast {{ 145 }}pts</p>
           </div>
           <Attributes
             v-model:wound="wound"
@@ -74,10 +71,23 @@ const pain = ref(5)
       <h2 class="h2 text-left">
         Average Attack Rolls
       </h2>
+      <table>
+        <tbody>
+          <tr>
+            <td class="p-1">
+              <label class="font-bold">Turns</label>
+            </td>
+            <td class="p-1">
+              <input v-model="turns" name="turns" class="input max-w-250px" type="number">
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
       <Combat
         :strength="strength"
         :toughness="toughness"
-        :attack="attack"
+        :attack="attack * turns"
         :accuracy="accuracy"
         :save="invulnerable"
         :damage="damage"
