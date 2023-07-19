9935d02e148c54b5d61f8efacca644b0adb2e8fa
Damien Robinson <damien.robinson@daim.dev>
2023-07-19 14:39:33 +1000

    feat(weapon attributes): add weapon profiles

--- components/WeaponAttributes.vue
+++ components/WeaponAttributes.vue
@@ -15,16 +15,28 @@ defineProps<{unit: Unit}>()
       <th class="p-2">D</th>
     </thead>
     <tbody>
-      <tr v-for="weapon of unit.weapons" :key="weapon.name" xclass="border-b-2 border-dashed">
-        <td class="p-2 text-center">{{ weapon.models ?? unit.models }}</td>
-        <td class="p-2">{{ weapon.name }}</td>
-        <td class="p-2 text-center">{{ weapon.range }}</td>
-        <td class="p-2 text-center">{{ weapon.attack }}</td>
-        <td class="p-2 text-center">{{ weapon.accuracy }}</td>
-        <td class="p-2 text-center">{{ weapon.strength }}</td>
-        <td class="p-2 text-center">{{ weapon.piercing }}</td>
-        <td class="p-2 text-center">{{ weapon.damage }}</td>
-      </tr>
+      <template v-for="weapon of unit.weapons" :key="weapon.name">
+        <tr v-for="profile of weapon.profiles" :key="profile.name" xclass="border-b-2 border-dashed">
+          <td class="p-2 text-center">{{ profile.models ?? weapon.models ?? unit.models }}</td>
+          <td class="p-2">{{ profile.name }}</td>
+          <td class="p-2 text-center">{{ profile.range }}</td>
+          <td class="p-2 text-center">{{ profile.attack }}</td>
+          <td class="p-2 text-center">{{ profile.accuracy }}</td>
+          <td class="p-2 text-center">{{ profile.strength }}</td>
+          <td class="p-2 text-center">{{ profile.piercing }}</td>
+          <td class="p-2 text-center">{{ profile.damage }}</td>
+        </tr>
+        <tr v-if="!weapon.profiles" xclass="border-b-2 border-dashed">
+          <td class="p-2 text-center">{{ weapon.models ?? unit.models }}</td>
+          <td class="p-2">{{ weapon.name }}</td>
+          <td class="p-2 text-center">{{ weapon.range }}</td>
+          <td class="p-2 text-center">{{ weapon.attack }}</td>
+          <td class="p-2 text-center">{{ weapon.accuracy }}</td>
+          <td class="p-2 text-center">{{ weapon.strength }}</td>
+          <td class="p-2 text-center">{{ weapon.piercing }}</td>
+          <td class="p-2 text-center">{{ weapon.damage }}</td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
