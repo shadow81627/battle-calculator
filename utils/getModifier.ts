a4fea4a05a08143d61dbf23cd90e2274a35b43ee
Damien Robinson <damien.robinson@daim.dev>
2023-07-29 16:08:02 +1000

    style(get modifiers): lint fix

--- utils/getModifier.ts
+++ utils/getModifier.ts
@@ -1,6 +1,6 @@
 export default function getModifier(name: string, modifiers: { name: string }[]) {
   return modifiers?.find(modifier =>
-    modifier.name.toUpperCase() === name.toUpperCase() ||
-    modifier.name.startsWith(name)
+    modifier.name.toUpperCase() === name.toUpperCase()
+    || modifier.name.startsWith(name),
   )
 }
