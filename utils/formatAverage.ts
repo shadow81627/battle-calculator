1c462372121fe64b4f927803e4711dc593de370d
Damien Robinson <damien.robinson@daim.dev>
2023-08-08 10:10:02 +1000

    feat(utils): add format average

---
+++ utils/formatAverage.ts
@@ -0,0 +1,6 @@
+export default function formatAverage(number: number) {
+  if (number % 1 === 0)
+    return number
+  const rounded = Math.round((number + Number.EPSILON) * 100) / 100
+  return Number(rounded).toFixed(2)
+}
