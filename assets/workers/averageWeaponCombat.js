5852b9828a6120538a93d9a3cb9c69b0521bd1bb
Damien Robinson <damien.robinson@daim.dev>
2023-09-11 13:25:34 +1000

    fix(average weapon combat worker): import average weapon combat util

--- assets/workers/averageWeaponCombat.js
+++ assets/workers/averageWeaponCombat.js
@@ -1,5 +1,6 @@
+import averageWeaponCombat from '~/utils/averageWeaponCombat'
+
 /* eslint-disable no-restricted-globals */
-/* eslint-disable no-undef */
 self.addEventListener('message', (e) => {
   const result = averageWeaponCombat(e.data)
   self.postMessage(result)
