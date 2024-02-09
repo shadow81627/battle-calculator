170cdcb43fa68910fb1ab77f499303fb7e614cbd
Damien Robinson <damien.robinson@daim.dev>
2024-02-09 18:40:57 +1000

    fix(fancy scribe page): add avoid page break css

--- pages/battlescribe/fancyscribe.vue
+++ pages/battlescribe/fancyscribe.vue
@@ -6,6 +6,22 @@
 </template>
 
 <script setup>
+definePageMeta({
+  layout: 'default',
+})
 const { data } = useFetch('/api/battlescribe/fancyscribe', { transform: (data) => data.data })
 const primaryColor = '#536766'
 </script>
+
+<style>
+@page {
+  margin: 0;
+  size: A4 landscape;
+}
+
+.avoid-page-break {
+  position: relative;
+  page-break-inside: avoid;
+  break-inside: avoid;
+}
+</style>
