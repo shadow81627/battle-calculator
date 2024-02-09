7c31e2c2cb035dd93135db4003e0d62d752af19d
Damien Robinson <damien.robinson@daim.dev>
2024-02-09 18:38:09 +1000

    fix(roster): replace print-display-none with print:hidden

--- components/FancyScribe/Roster.vue
+++ components/FancyScribe/Roster.vue
@@ -6,7 +6,7 @@ const { name, cost, forces } = props.roster
 <template>
   <div style="position: relative">
     <div
-      class="print-display-none" style="background-color: var(--primary-color);color: #fff;padding: 4px 16px;font-size:  1.7em;font-weight: 800;text-transform: uppercase;margin-bottom: 12px;"
+      class="print:hidden" style="background-color: var(--primary-color);color: #fff;padding: 4px 16px;font-size:  1.7em;font-weight: 800;text-transform: uppercase;margin-bottom: 12px;"
     >
       {{ name }} [{{ cost.points }} pts]
     </div>
