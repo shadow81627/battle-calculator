248dffd8090afe7a665edfddf6fdc9165384f0b2
Damien Robinson <damien.robinson@daim.dev>
2024-02-13 09:45:53 +1000

    fix(side nav): add scrim

--- components/SideNav.vue
+++ components/SideNav.vue
@@ -3,7 +3,7 @@
     style="left: 0px; bottom: 0px; top: 64px; width: 256px; height: calc(100% - 64px); position: fixed; z-index: 1006; transition-timing-function: cubic-bezier(.4,0,.2,1);"
     class="fixed flex grow min-h-full shadow-md bg-white dark:bg-dark flex-col transition-transform duration-200 !print:hidden overflow-hidden"
     :class="{ 'translate-x-0': draw, 'translate-x-[-110%]': !draw }">
-    <div v-for="item of items" :key="item.name" class="flex px-4" @click="() => $emit('update:draw', !draw)"
+    <div v-for="item of items" :key="item.name" class="flex px-4" @click="close"
       :class="{ 'bg-gray-200': $route.path === item.url, 'dark:bg-gray-600': $route.path === item.url, 'hover:bg-gray-500/10': $route.path !== item.url }">
       <Icon :name="item.icon" class="text-gray my-auto" style="width: 32px; height: 32px" />
       <NuxtLink :to="item.url" itemscope itemtype="https://schema.org/SiteNavigationElement"
@@ -11,6 +11,9 @@
         <slot>{{ item.name }}</slot>
       </NuxtLink>
     </div>
+  </div>
+  <div v-if="draw && isSmallScreen" class="opacity-20" @click="close"
+    style="background: #000;height: 100%;left: 0;position: absolute;top: 0;transition: opacity 0.2s cubic-bezier(.4,0,.2,1);width: 100%;z-index: 1005;">
   </div>
 </template>
 <script>
@@ -25,8 +28,8 @@ export default {
     items: { type: Array, default: () => [] },
   },
   methods: {
-    itemClick() {
-      if (this.isSmallScreen.value) {
+    close() {
+      if (this.isSmallScreen) {
         this.$emit('update:draw', false)
       }
     }
