62f3d6687648710377f3366903d2d35b5d2083cb
Damien Robinson <damien.robinson@daim.dev>
2024-02-13 09:21:52 +1000

    fix(pwa): remove unused icons

--- config/pwa.ts
+++ config/pwa.ts
@@ -16,6 +16,11 @@ export const pwa: ModuleOptions = {
     theme_color: '#ffffff',
     icons: [
       {
+        src: 'pwa-64x64.png',
+        sizes: '64x64',
+        type: 'image/png',
+      },
+      {
         src: 'pwa-192x192.png',
         sizes: '192x192',
         type: 'image/png',
@@ -26,7 +31,7 @@ export const pwa: ModuleOptions = {
         type: 'image/png',
       },
       {
-        src: 'maskable-icon.png',
+        src: 'maskable-icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any maskable',
