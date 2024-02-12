49ab07ff11cf0de1b8d28d46336d70902fafea21
Damien Robinson <damien.robinson@daim.dev>
2024-02-12 15:47:25 +1000

    chore: generate pwa icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,8 +44,8 @@ export default defineNuxtConfig({
       viewport: 'width=device-width,initial-scale=1',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon-180x-180.png' },
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
