c660e30f926158f1e1ceefe8331a39e618e4c687
Damien Robinson <damien.robinson@daim.dev>
2024-02-21 11:09:02 +1000

    fix(combat workers): return id

--- assets/workers/averageWeaponCombat.js
+++ assets/workers/averageWeaponCombat.js
@@ -3,5 +3,5 @@ import averageWeaponCombat from '~/utils/averageWeaponCombat'
 /* eslint-disable no-restricted-globals */
 self.addEventListener('message', (e) => {
   const result = averageWeaponCombat(e.data)
-  self.postMessage(result)
+  self.postMessage({ ...result, id: e.data.id })
 }, false)
