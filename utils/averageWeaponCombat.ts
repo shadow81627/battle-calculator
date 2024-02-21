c323368a556fb3c7be594aecbc94dfadbfcd554a
Damien Robinson <damien.robinson@daim.dev>
2024-02-21 11:09:39 +1000

    fix(average weapon combat): sum other averages

--- utils/averageWeaponCombat.ts
+++ utils/averageWeaponCombat.ts
@@ -1,8 +1,6 @@
-import formatAverage from './formatAverage'
 import type CombatModifiers from '~/types/CombatModifiers'
 import type Unit from '~/types/unit'
 import type { Weapon } from '~/types/unit'
-import weaponCombat from '~/utils/weaponCombat'
 
 const defaultResult = {
   randomAttacksTotal: 0,
@@ -17,9 +15,10 @@ const defaultResult = {
 
 function resultSum(sum: typeof defaultResult, result: typeof defaultResult) {
   for (const [key, value] of Object.entries(result)) {
+    const _key = key.replace('average', 'random')
     const isResultKey = (key: any): key is keyof typeof defaultResult => key in defaultResult
-    if (isResultKey(key) && typeof value === 'number')
-      sum[key] = sum[key] + value
+    if (isResultKey(_key) && typeof value === 'number')
+      sum[_key] += value
   }
   return sum
 }
@@ -29,11 +28,12 @@ export default function averageWeaponCombat(data: {
   unit: Unit
   target: Unit
   additional: CombatModifiers
+  numberOfRolls: 100
+  rolls: typeof defaultResult[]
 }) {
-  const numberOfRolls = 10_000
-  const rolls = [...Array(numberOfRolls)].map(() => weaponCombat(data.weapon, data.unit, data.target, data.additional))
+  const rolls = data.rolls?.length ? data.rolls : [...Array(data.numberOfRolls)].map(() => weaponCombat(data.weapon, data.unit, data.target, data.additional))
   const sums = Object.entries(rolls.reduce(resultSum, { ...defaultResult }))
-  const averages = Object.fromEntries(sums.map(([sumKey, sumValue]) => [sumKey.replace('random', 'average'), formatAverage(sumValue / rolls.length)]))
+  const averages = Object.fromEntries(sums.map(([sumKey, sumValue]) => [sumKey.replace('random', 'average'), (sumValue / rolls.length)]))
   // const probabilities = Object.fromEntries(Object.entries(occurrences(rolls.reduce((acc, roll) => {
   //   return acc.concat(roll.randomHitRolls)
   // }, []))).map(([key, value]) => [key, formatAverage(value / rolls.length)]))
