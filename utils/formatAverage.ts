cd7c65c7ebcc946ce9b4a3204ee5e783bbb05f7c
Damien Robinson <damien.robinson@daim.dev>
2024-02-21 12:19:40 +1000

    fix(format average): only 1 decimal place

--- utils/formatAverage.ts
+++ utils/formatAverage.ts
@@ -1,5 +1,5 @@
 export default function formatAverage(number: number) {
   if (number % 1 === 0) return number;
   const rounded = Math.round((number + Number.EPSILON) * 100) / 100;
-  return Number(rounded).toFixed(2);
+  return Number(rounded).toFixed(1);
 }
